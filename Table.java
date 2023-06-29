f83a97a07f74ce61efc7c6da8aba3a3efd931d49
deadredraccoon21 <97346312+deadredraccoon21@users.noreply.github.com>
2023-06-29 09:25:10 +0530

    constructor-push

---
+++ Table.java
@@ -0,0 +1,52 @@
+public class Table{
+	static int salary;
+	static long phno;
+	static float cgpa;
+	static double pi;
+	static char grade;
+	public Table(){
+		System.out.println("This is a default contructor");
+	}
+	public Table(int salary1, long phno1, float cgpa1, double pi1, char grade1){
+		System.out.println("This is a parameterized contructor");
+		salary=salary1;
+		phno=phno1;
+		cgpa=cgpa1;
+		pi=pi1;
+		grade=grade1;
+	}
+	public static void main(String[] args){
+		Table dc = new Table();
+		Table pc = new Table(50000,9353617814L,7.4f,3.1416329,'A');
+		System.out.println("salary:"+salary);
+		System.out.println("phone no.:"+phno);
+		System.out.println("cgpa:"+cgpa);
+		System.out.println("pi:"+pi);
+		System.out.println("grade:"+grade);
+	}
+}
+		
+		
+		;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
